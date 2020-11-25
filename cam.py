ac184c9306cc0136111a224b5773bf9c896cee7b
l0nl1f3 <l0nl1f3@gmail.com>
2020-11-25 14:14:47 +0800

    Lab7

---
+++ cam.py
@@ -0,0 +1,12 @@
+import cv2 # 加载 OpenCV 库
+cam = cv2.VideoCapture(0) # 打开摄像头
+cam.set(3, 1024) # 设置图像宽度
+cam.set(4, 768) # 设置图像高度
+while(True):
+    ret, frame = cam.read() # 读入一帧图像
+    cv2.imshow('Video Test',frame) # 显示图像
+    if cv2.waitKey(1) == ord("q") : # 等待按键
+        break
+
+cam.release()
+cv2.destroyAllWindows()
